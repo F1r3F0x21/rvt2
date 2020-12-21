01f1e463da9aa27a0862730f4aa5df3fbbf07d6e
pgarcia <pgarcia@incide.es>
2020-12-21 13:55:44 +0100

    iOS WhatsApp and characterization additions

--- plugins/ios/unback.py
+++ plugins/ios/unback.py
@@ -34,9 +34,9 @@ import base.utils
 
 class Unback(plugins.ios.IOSModule):
     """
-    Unback an ios backup directory into a directory.
+    Unback an iOS backup directory or zip file containing an iOS backup.
 
-    Currently, only unencrypted backups are supported.
+    In case of encrypted backups, an external unback command must be provided.
 
     Configuration:
         - **unzip_path**: If needed, unzip the source to this path before unbacking.
@@ -45,6 +45,7 @@ class Unback(plugins.ios.IOSModule):
           It is a Python string template that receives variables "bk_path" and "extract_path".
           An external command might be useful to unback encrypted backups.
           For example, check https://github.com/dinosec/iphone-dataprotection/blob/master/python_scripts/backup_tool.py
+        - **remove_unzip_path**: If set to True (default), delete the unzip directory after unzipping the backup zip file
     """
 
     def read_config(self):
@@ -55,7 +56,7 @@ class Unback(plugins.ios.IOSModule):
         self.set_default_config('unback_cmd', '')
 
     def _get_backup_directory(self, path):
-        """ Try to guess the iIOS backup directory.
+        """ Try to guess the iOS backup directory.
 
         - If the path is a directory and it includes a file Info.plist, assume it is a backup directory
         - If the path is a zip file and the directory does not exist yet, extract the zip into unzip_path
@@ -109,17 +110,28 @@ class Unback(plugins.ios.IOSModule):
 
         Returns:
             An empty array, always.
+
+        Raises:
+            base.job.RVTError if the file can't be unbacked, for whatever reason
         """
+
         self.check_params(path, check_path=True, check_path_exists=True)
         self.logger().info('Unback: %s', path)
         # check unzip path
         if base.utils.check_directory(self.myconfig('unzip_path')):
-            shutil.rmtree(self.myconfig('unzip_path'))
+            try:
+                shutil.rmtree(self.myconfig('unzip_path'))
+            except PermissionError:
+                self.logger().warning('Can\'t remove: {}. I will try to continue'.format(self.myconfig('unzip_path')))
+            except OSError:
+                # for example: directory is busy
+                self.logger().warning('Can\'t remove: {}. I will try to continue'.format(self.myconfig('unzip_path')))
         extract_path = self.myconfig('extract_path')
         # check extract_path
         if base.utils.check_directory(extract_path):
-            # If already unbacked, exit
-            return []
+            # If already unbacked
+            # return []
+            pass
         # create the extract_path directory, if it does not exist
         base.utils.check_directory(extract_path, create=True, delete_exists=True)
         self.logger().debug('Extracting to: %s', extract_path)
@@ -158,6 +170,12 @@ class Unback(plugins.ios.IOSModule):
 
         # finally, if the unzip path exists, remove it
         if self.myflag('remove_unzip_path') and base.utils.check_directory(self.myconfig('unzip_path')):
-            shutil.rmtree(self.myconfig('unzip_path'))
+            try:
+                shutil.rmtree(self.myconfig('unzip_path'))
+            except PermissionError:
+                self.logger().warning('Can\'t remove: {}'.format(self.myconfig('unzip_path')))
+            except OSError:
+                # for example: directory is busy
+                self.logger().warning('Can\'t remove: {}'.format(self.myconfig('unzip_path')))
 
         return []
