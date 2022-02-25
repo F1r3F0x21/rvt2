80ca6dc6d15f2bb7cb5145339cef7ba0669210c6
pgarcia <pgarcia@incide.es>
2022-02-25 13:47:57 +0100

    Added parser for some Office 365 logs

---
+++ plugins/common/RVT_365.py
@@ -0,0 +1,54 @@
+# Copyright (C) INCIDE Digital Data S.L.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import json
+import base.job
+
+class Parse_Audit_Logs(base.job.BaseModule):
+    """ 
+        Office 365 Audit Logs original CSV output contains a json format field.
+        This job parses that field and yield the results
+    """
+
+    def run(self, path=None):
+        self.check_params(path, check_path=True, check_path_exists=True)
+
+        for row in self.from_module.run(path):
+            data = {}
+            audit_data = row['AuditData']
+            audit_data = json.loads(audit_data)
+            data["CreationTime"] = audit_data["CreationTime"]
+            data["Operation"] = audit_data["Operation"]
+            data["User"] = audit_data["UserId"]
+            data["ClientIP"] = audit_data["ClientIP"] if "ClientIP" in audit_data else ""
+            data["LogonError"] = audit_data["LogonError"] if "LogonError" in audit_data else ""
+            
+            data["SessionId"] = ""
+            if "DeviceProperties" in audit_data:
+                for property in audit_data["DeviceProperties"]:
+                    if property["Name"] == "SessionId":
+                        data["SessionId"] = property["Value"]
+                        break
+            
+            data["UserAgent"] = ""
+            if "ExtendedProperties" in audit_data:
+                for property in audit_data["ExtendedProperties"]:
+                    if property["Name"] == "UserAgent":
+                        data["UserAgent"] = property["Value"]
+                        break
+
+            yield data
+
+
