069350dd0a6c78b21dc9a7a1829793533d028001
Sergi Ger <sger@incide.es>
2023-12-11 13:42:59 +0100

    Linux AnalysisLinuxAptHistoryLog

--- plugins/linux/RVT_extra_logs.py
+++ plugins/linux/RVT_extra_logs.py
@@ -15,11 +15,11 @@
 
 import os
 import re
+import ast
 import pandas as pd
 import base.job
 from datetime import datetime
-from base.utils import date_to_iso
-
+from base.utils import check_directory, date_to_iso, save_csv
 from plugins.linux import get_timezone
 
 
@@ -122,9 +122,25 @@ class LinuxAptHistoryLog(base.job.BaseModule):
                 linesplited = line.split(":", 1)
                 if linesplited[0] == "Start-Date":
                     aux_dict = {}
-                aux_dict[linesplited[0]] = linesplited[1]
-                if linesplited[0] == "End-Date":
-                    yield aux_dict
+                    timestamp = linesplited[1].strip()
+                    localdate = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+                    isodate = date_to_iso(localdate, input_timezone = get_timezone(self.myconfig('mountdir')))
+                    aux_dict["@timestamp"] = isodate
+                elif linesplited[0] == "End-Date":
+                        timestamp = linesplited[1].strip()
+                        localdate = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+                        isodate = date_to_iso(localdate, input_timezone = get_timezone(self.myconfig('mountdir')))
+                        aux_dict[linesplited[0]] = isodate
+                        yield aux_dict
+                elif linesplited[0] == "Commandline":
+                    aux_dict[linesplited[0]] = linesplited[1]
+                else:
+                    if "action" in aux_dict:
+                        aux_list =  list(aux_dict["action"])
+                        aux_list.append({linesplited[0]:linesplited[1]})
+                        aux_dict["action"] = aux_list
+                    else:
+                        aux_dict["action"] = [{linesplited[0]:linesplited[1]}]
 
 
 class AnalysisLinuxSshLog(base.job.BaseModule):
@@ -199,207 +215,70 @@ class AnalysisLinuxSshLog(base.job.BaseModule):
                         df_sshlogin_aux.at[index, "ut_time_total"] = formatted_result
                         yield df_sshlogin_aux.loc[index, ['@timestamp', 'host.hostname','user.name', 'method', 'ut_host', 'port', 'ut_time_to', 'ut_time_total']].to_dict()
 
-''' 
-class LinuxStandardLog2(base.job.BaseModule):
-    
-    """ Extract the Auth.Log file
+
+class AnalysisLinuxAptHistoryLog(base.job.BaseModule):
+    """ Analisis the Ssh log
 
     Module description:
         - **from_module**: Data dict.
         - **yields**: The updated dict data.
     """
-
-    def read_config(self):
-        super().read_config() 
-
-    def run(self, path=None):
-        self.logger().warning("Please note that reading " + os.path.basename(path) + " could take some time")
-
-        pattern = r'(\w+\s+\d+\s\d+:\d+:\d+)\s([\w.-]+)\s(\S+):(\s.*)?'
-        prog = re.compile(pattern)
-        tz = get_timezone(self.myconfig('mountdir'))
-
-
-        prev_date_str = "Jan 1 00:00:00"
-        prev_date = datetime.strptime(prev_date_str, "%b %d %H:%M:%S")
-        year_passed = 0
-
-        result = subprocess.run(["exiftool", "-ModifyDate", path], capture_output=True, text=True, check=True)
-        print(result.stdout)
-
-
-        modification_time = os.path.getmtime(path)
-        year = time.localtime(modification_time).tm_year
-
-        filename = os.path.basename(path)
-
-        for line in self.from_module.run(path):
-            match = prog.match(line)
-            if match:
-                timestamp, host, process, command = match.groups()
-                log_entry_dict = {
-                    "@timestamp": timestamp,
-                    "host.hostname": host,
-                    "process.name": process,
-                    "process.command_line": command,
-                    "filename": filename
-
-                }
-                actual_date = datetime.strptime(timestamp, "%b %d %H:%M:%S")
-                if ((prev_date.month, prev_date.day) > (actual_date.month, actual_date.day)):
-                    year_passed += 1
-                prev_date = actual_date
-
-                log_timestamp_with_year = f"{year + year_passed} {log_entry_dict['@timestamp']}"
-
-                # Parse the timestamp and convert it to ISO format
-                parsed_timestamp = datetime.strptime(log_timestamp_with_year, "%Y %b %d %H:%M:%S")     
-                output_string_utc = date_to_iso(parsed_timestamp, input_timezone=tz, output_timezone="UTC")
-                log_entry_dict['@timestamp'] = output_string_utc
-                yield log_entry_dict
-
-            else:
-                self.logger().warning("Regex pattern failed with some logline input " + line)
-
-class CSVReaderReversed(base.job.BaseModule):
-    """ Yields every line in a CSV file.
-
-    Configuration:
-        - **encoding** (String): The encoding to use. Defaults to "utf-8"
-        - **delimiter** (String): The delimiter to use. Use `AUTO` to dinamically find out. Defaults to ;
-        - **quotechar** (String): The quotechar. Defaults to \"
-        - **restkey** (String): The restkey of the DictReader. Defaults to "extra".
-        - **restval** (String): The restval of the DictReader. Defaults to the empty string.
-        - **fieldnames**: A space separated list of header names. If None, use the first line.
-          Warning: If provided, the first line will be considered data unless ignore_lines is set to >0
-        - **ignore_lines** (int): Ignore this number of initial lines. If fieldnames is provided, the first line is also ignored.
-        - **progress.disable** (Boolean): If True, disable the progress bar.
-        - **progress.cmd** (String): The shell command to run to estimate the number of lines in the file.
-        - **check_path_exists** (Boolean): If True and provided path does not exist, raise an error. I False, just warn and continue
-    """
-
     def read_config(self):
         super().read_config()
-        self.set_default_config('encoding', 'utf-8')
-        self.set_default_config('delimiter', ';')
-        self.set_default_config('quotechar', '"')
-        self.set_default_config('restkey', 'extra')
-        self.set_default_config('restval', '')
-        self.set_default_config('fieldnames', '')
-        self.set_default_config('ignore_lines', '0')
-        self.set_default_config('progress.disable', 'False')
-        self.set_default_config('progress.cmd', 'cat "{path}" | wc -l')
-        self.set_default_config('field_size_limit', sys.maxsize)  # Default csv max is 131072
-        self.set_default_config('check_path_exists', True)
-
-
-    def run(self, path):
-        """ Read CSV file in the path. from_module is ignored """
-        try:
-            self.check_params(path, check_path=True, check_path_exists=True)
-        except base.job.RVTErrorNotExistingPath as exc:
-            if not self.myflag('check_path_exists'):
-                self.logger().warning(exc)
-                return []
-            raise exc
-        csv.field_size_limit(int(self.myconfig('field_size_limit')))
-        with open(path, 'r', encoding=self.myconfig('encoding')) as infile:
-            ignore_lines = int(self.myconfig('ignore_lines'))
-            fieldnames = self.myarray('fieldnames', None)
-            for i in range(0, ignore_lines):
-                infile.readline()
-            if self.myconfig('delimiter') == 'AUTO':
-                delimiter = csv.Sniffer().sniff(infile.readline()).delimiter
-                infile.seek(0)
-            else:
-                delimiter = self.myconfig('delimiter')
-            reader = csv.DictReader(
-                infile,
-                fieldnames=fieldnames,
-                restval=self.myconfig('restval'), restkey=self.myconfig('restkey'),
-                delimiter=delimiter, quotechar=self.myconfig('quotechar'))
-            # progress management
-            total_iterations = estimate_iterations(path, self.myconfig('progress.cmd'))
-            # if fieldnames is None, the first line is header. Add one to progress
-            if fieldnames:
-                initial_progress = ignore_lines
-            else:
-                initial_progress = ignore_lines + 1
-            # main loop
-            for data in tqdm(reversed(list(reader)), total=total_iterations,
-                             initial=initial_progress,
-                             desc='Reading {}'.format(os.path.basename(path)),
-                             disable=self.myflag('progress.disable')):
-                yield data
-
-class AllLinesInFileReversed(base.job.BaseModule):
-    """ Yields every line in a file as a string
-
-    Configuration:
-        - **encoding** (String): The encoding to use. Defaults to utf-8
-        - **progress.disable** (Boolean): If True, disable the progress bar.
-        - **progress.cmd** (String): The shell command to run to estimate the number of lines in the file. """
-
-    def read_config(self):
-        super().read_config()
-        self.set_default_config('encoding', 'utf-8')
-        self.set_default_config('progress.disable', 'False')
-        self.set_default_config('progress.cmd', 'cat "{path}" | wc -l')
-
-    def run(self, path):
-        """ Read all lines from the path. from_module is ignored """
-        self.check_params(path, check_path=True, check_path_exists=True)
-        total_iterations = base.commands.estimate_iterations(path, self.myconfig('progress.cmd'))
-        with open(path, 'r', encoding=self.myconfig('encoding')) as infile:
-            for line in tqdm(reversed(list(infile)), total=total_iterations,
-                             desc='Reading {}'.format(os.path.basename(path)),
-                             disable=self.myflag('progress.disable')):
-                yield line.strip()
-
-class YearLinuxStandardLog2(base.job.BaseModule):
-    """ Extract the Auth.Log file
-
-    Module description:
-        - **from_module**: Data dict.
-        - **yields**: The updated dict data.
-    """
-
-    def read_config(self):
-        self.set_default_config('log_files', None)
-        super().read_config() 
     
     def run(self, path=None):
-        log_files = self.myconfig('log_files')
-        list_log_files = glob.glob(log_files, recursive=self.myflag('recursive'))
-        list_log_files_sorted = sorted(list_log_files)
-
-
-        tz = get_timezone(self.myconfig('mountdir'))
-        prev_date_str = "Jan 1 00:00:00"
-        prev_date = datetime.strptime(prev_date_str, "%b %d %H:%M:%S")
-        year_passed = 0
-
-        modification_time = os.path.getmtime(list_log_files_sorted[0])
-        year = time.localtime(modification_time).tm_year
+        pkg_pattern = r'([\w\.-]+):(.+)\s\(([\d\~\w\.-]*).*\)'
+        pkg_prog = re.compile(pkg_pattern)
+        upgrade_list = []
+        remove_list = []
+        purge_list = []
 
         for line in self.from_module.run(path):
-            # Read a single line
-            log_entry_dict = {
-                "@timestamp": line["@timestamp"],
-                "host.hostname": line["host.hostname"],
-                "process.name": line["process.name"],
-                "process.command_line": line["process.command_line"]
-            }
-            actual_date = datetime.strptime(line["@timestamp"], "%b %d %H:%M:%S")
-            if ((prev_date.month, prev_date.day) < (actual_date.month, actual_date.day)):
-                year_passed -= 1
-            prev_date = actual_date
+            user_responsible = '' 
+            action_list = ast.literal_eval(line["action"])
+            if any("Requested-By" in x for x in action_list):
+                user_responsible = [x["Requested-By"] for x in action_list if "Requested-By" in x]
 
-            log_timestamp_with_year = f"{year + year_passed} {log_entry_dict['@timestamp']}"
+            for action in action_list:
+                package_action, package = list(action.items())[0]
+                if not package_action == "Requested-By":
+                    for package_name in package.split("),"):
+                        if not str(package_name).endswith(")"):
+                            package_name += ")"
+                        match_pkg = pkg_prog.match(package_name.strip())
+                        if match_pkg:
+                            package_name, package_architecture, package_version = match_pkg.groups()
+                            data_dict = {
+                                '@timestamp': line['@timestamp'],
+                                'package.name' : package_name,
+                                'package.architecture' : package_architecture,
+                                'package.version' : package_version,
+                                'username' : user_responsible
+                            }
 
-            #Parse the timestamp and convert it to ISO format
-            parsed_timestamp = datetime.strptime(log_timestamp_with_year, "%Y %b %d %H:%M:%S")     
-            output_string_utc = date_to_iso(parsed_timestamp, input_timezone=tz, output_timezone="UTC")
-            log_entry_dict['@timestamp'] = output_string_utc
-            yield log_entry_dict
-'''
+                            if package_action == "Install":
+                                yield data_dict
+                            elif package_action == "Upgrade":
+                                upgrade_list.append(data_dict)
+                            elif package_action == "Remove":
+                                remove_list.append(data_dict)
+                            elif package_action == "Purge":
+                                purge_list.append(data_dict)
+                        else:
+                            self.logger().warning("Regex pattern failed with some package name: " + package_name)
+
+        # Save upgraded, removed and purged packages in diferent csv
+        analysisdir = self.myconfig('analysisdir')
+        check_directory(analysisdir, create=True)
+
+        if upgrade_list:
+            csv_upgrade_out = os.path.join(analysisdir, 'apt_packages_upgraded.csv')
+            save_csv(upgrade_list, outfile=csv_upgrade_out)
+        
+        if remove_list:
+            csv_remove_out = os.path.join(analysisdir, 'apt_packages_removed.csv')
+            save_csv(remove_list, outfile=csv_remove_out)
+
+        if purge_list:
+            csv_purge_out = os.path.join(analysisdir, 'apt_packages_purged.csv')
+            save_csv(purge_list, outfile=csv_purge_out)
